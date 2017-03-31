2311494cee2cac1baf4cfaab320b67a5cbb955e8
Christian Nolte <hello@noltech.net>
2017-04-01 00:26:35 +0200

    Remove arg parsing.

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ MAINTAINER Christian Nolte hello@noltech.net
 
 COPY Caddyfile /etc/Caddyfile
 COPY docker-entrypoint.sh /entrypoint.sh
+
 ENTRYPOINT ["/entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,24 +2,8 @@
 
 set -e
 
-caddyfile=""
-
-while [ $# -gt 0 ]; do
-  key="$1"
-
-  case $key in
-      -conf|--conf)
-        caddyfile="$2"
-        shift
-      ;;
-      *)
-        shift
-      ;;
-  esac
-done
-
-if [ ! -e "$caddyfile" ]; then
-  cp /etc/Caddyfile "$caddyfile"
+if [ ! -z "$CADDYFILE" ]; then
+  cp /etc/Caddyfile "$CADDYFILE"
 fi
 
 exec /usr/bin/caddy "$@"
